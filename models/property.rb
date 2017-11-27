7b75c17dfc63811a09f1809f95cef94fafb6fe97
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-11-27 15:49:19 +0000

    now with one table

--- models/property.rb
+++ models/property.rb
@@ -1,3 +1,11 @@
 class Property
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :description, Text
+  property :price, Integer
+  property :email, String
+
 
 end
