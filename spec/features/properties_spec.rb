f12577e62aacc72c0151a6abe98206af09a17249
Eva.Dinckel <evadinckel@gmail.com>
2017-11-28 12:26:00 +0000

    make email as link with anchor tag, add the Id property in the property erb file

--- spec/features/properties_spec.rb
+++ spec/features/properties_spec.rb
@@ -1,7 +1,8 @@
 feature 'List of properties' do
   scenario 'User can see properties' do
     post_property
+    property_id = Property.all.last.id
     visit('/properties')
-    expect(page).to have_content("Name: T-Hotel Description: Awesome Price: £60 Email: 1234@try.it ")
+    expect(page).to have_content("Id: #{property_id} Name: T-Hotel Description: Awesome Price: £60 Email: 1234@try.it ")
   end
 end
