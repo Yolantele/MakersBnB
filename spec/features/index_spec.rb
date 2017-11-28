0ea575d9c3d918b6c40a1236c586768c4fce7d20
Tom Moir <trmoir9@gmail.com>
2017-11-28 09:42:06 +0000

    minor changes to index files and spec files to fix tests

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,9 +1,7 @@
-
 feature 'visiting the home page' do
   scenario 'user can go to the home page' do
     visit('/home')
     expect(page).to have_content('User email:', 'User password:')
     expect(page).to have_content('Owner email:', 'Owner password:')
-    end
   end
 end
--- spec/features/traveler_spec.rb
+++ spec/features/traveler_spec.rb
@@ -1,12 +1,9 @@
-feature 'visiting the home page' do
-
-    scenario 'user can go to the home page' do
-      visit('/home')
-      fill_in(:user_email, with: 'email')
-      fill_in(:user_password, with: 'password')
-      click_button('Enter')
-      expect(page).to have_content('traveller side')
-    end
+feature 'Traveller Home' do
+    scenario 'user can fill in traveler form' do
+    visit('/home')
+    fill_in(:user_email, with: 'email')
+    fill_in(:user_password, with: 'password')
+    click_button(id: 'traveller_enter')
+    expect(page).to have_content('traveller side')
   end
-
 end
